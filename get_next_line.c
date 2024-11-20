5f578fb7d13ac5e1226deb020347adc64c2e5bcf
Mohamed Dahani <mdahani@e1r3p12.1337.ma>
2024-11-20 20:11:12 +0100

    start on the project

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,26 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 10:24:10 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/08 10:25:01 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/20 16:45:32 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*get_next_line(int fd)
+#include "get_next_line.h"
+
+void	create_list(t_list **list, int fd)
 {
 	
+}
+
+char	*get_next_line(int fd)
+{
+	static t_list	*list;
+	char			*next_line;
+
+	list = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
+		return (NULL);
+	create_list(&list, fd);
+	if (!list)
+		return (NULL);
 }
---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 14:43:44 by mdahani           #+#    #+#             */
+/*   Updated: 2024/11/20 16:44:30 by mdahani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	char			*buf;
+	struct s_list	*next;
+}					t_list;
+
+char	*get_next_line(int fd);
+
+#endif
