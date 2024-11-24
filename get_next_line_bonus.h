817b73f4ddd67f5098bcd0dcb82302a9a6c1cdef
Mohamed Dahani <mdahani@e1r11p12.1337.ma>
2024-11-24 18:50:32 +0100

    finish the bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,25 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 18:21:41 by mdahani           #+#    #+#             */
-/*   Updated: 2024/11/24 18:21:42 by mdahani          ###   ########.fr       */
+/*   Updated: 2024/11/24 18:39:55 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(char *s1);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *str);
+#endif
